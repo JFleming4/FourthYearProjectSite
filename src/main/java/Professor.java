8f33666e7fcbcb799d165e11c1158309edc0fc01
Noah Segal <nsegal91@icloud.com>
2018-03-05 16:18:43 -0500

    Added initial implementation of objects.

---
+++ src/main/java/Professor.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+public class Professor extends User {
+
+    private ArrayList<Project> projects;
+    private ArrayList<Project> secondReaderProjects;
+    private ProjectCoordinator projectCoordinator;
+
+
+    public Professor(String firstName, String lastName, String email)
+    {
+        super(firstName, lastName, email);
+        this.projects = new ArrayList<>();
+    }
+
+
+    /**
+     * Create a new project and add it to the Projects list of the calling Professor and
+     * the Project Coordinator.
+     * @param description Project description
+     * @param restrictions Project restrictions
+     * @param studentCapacity Project capacity (how many students can join)
+     */
+    public void createProject(String description, ArrayList<String> restrictions, int studentCapacity)
+    {
+        Project project = new Project(this, description, restrictions, studentCapacity);
+
+        projectCoordinator.addProjectToList(project);
+        this.addProjectToList(project);
+    }
+
+
+    /**
+     * Delete the project, removing it from the Professor's and the Project Coordinator's
+     * list of proejcts.
+     * @param project The project to delete
+     */
+    public void deleteProject(Project project)
+    {
+        if (projects.contains(project))
+        {
+            projectCoordinator.removeProjectFromList(project); // Should we do this?
+            this.removeProjectFromList(project);
+
+            project.setIsDeleted(true);
+        }
+    }
+
+
+    /**
+     * Archive the project, but keep it in the Professor's list of projects
+     * @param project The project to archive
+     */
+    public void archiveProject(Project project)
+    {
+        if (projects.contains(project))
+            project.setIsArchived(true);
+    }
+
+
+    /**
+     * Compare an unknown object to this Professor object
+     * @param obj Unknown object
+     * @return Boolean whether or not the objects are the same
+     */
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+
+        if (!(obj instanceof Professor)) return false;
+
+        Professor prof = (Professor) obj;
+
+        return super.equals(obj)
+                && this.projects.equals(prof.getProjects())
+                && this.secondReaderProjects.equals(prof.secondReaderProjects)
+                && (this.getProjectCoordinator().equals(prof.getProjectCoordinator()));
+    }
+
+
+    protected void addProjectToList(Project project) {
+        projects.add(project);
+    }
+
+    protected void removeProjectFromList(Project project) {
+        projects.remove(project);
+    }
+
+    protected void addProjectToSecondReaderList(Project project) {
+        secondReaderProjects.add(project);
+    }
+
+    protected void removeProjectFromSecondReaderList(Project project) {
+        secondReaderProjects.remove(project);
+    }
+
+
+    ///////////////
+    // Get & Set //
+    ///////////////
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
+
+    public ArrayList<Project> getSecondReaderProjects() {
+        return secondReaderProjects;
+    }
+
+    public void setSecondReaderProjects(ArrayList<Project> secondReaderProjects) {
+        this.secondReaderProjects = secondReaderProjects;
+    }
+
+    public ProjectCoordinator getProjectCoordinator() {
+        return projectCoordinator;
+    }
+
+    public void setProjectCoordinator(ProjectCoordinator projectCoordinator) {
+        this.projectCoordinator = projectCoordinator;
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,67 @@
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void enterAvailability()
+    {
+        // should we add an availability attribute?
+    }
+
+
+    /**
+     * Compare an unknown object to this User object
+     * @param obj Unknown object
+     * @return Boolean whether or not the objects are the same
+     */
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+
+        if (obj != null && obj.getClass() == this.getClass())
+        {
+            User user = (User) obj;
+            return this.firstName.equals(user.getFirstName())
+                    && this.lastName.equals(user.getLastName())
+                    && this.email.equals(user.getEmail());
+        }
+        return false;
+    }
+
+
+    ///////////////
+    // Get & Set //
+    ///////////////
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
