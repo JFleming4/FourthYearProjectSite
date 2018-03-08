c8e3238667291096ae7cc70401fd41984ac3079c
Justin Krol <justin.krol@shopify.com>
2018-03-07 23:13:54 -0500

    Create a barebones student view page

--- src/main/java/app/controllers/HomeController.java
+++ src/main/java/app/controllers/HomeController.java
@@ -1,12 +1,38 @@
 package app.controllers;
 
+import app.models.Professor;
+import app.models.Project;
+import app.models.ProjectCoordinator;
+import app.models.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
-    @GetMapping("/hello")
-    public String home() {
-        return "home";
+    private Student student1, student2;
+    private ProjectCoordinator coordinator;
+    private Professor prof1, prof2;
+    private Project project;
+
+    @GetMapping("/student")
+    public String student(Model model) {
+        // This is some sample data until the db is hooked up
+        Student student1 = new Student("Justin", "Krol", "justinkrol@cmail.carleton.ca", 100990999, "Software Engineering");
+        Student student2 = new Student("Derek", "Stride", "derekstride@cmail.carleton.ca", 100440444, "Software Engineering");
+        ProjectCoordinator coordinator = new ProjectCoordinator("Sir", "Coordinate", "coordinator@sce.carleton.ca");
+        Professor prof1 = new Professor("Babak", "Esfandiari", "babak@sce.carleton.ca", coordinator);
+        Professor prof2 = new Professor("Samuel", "Ajila", "ajila@sce.carleton.ca", coordinator);
+        Project project = new Project(prof1, "GraphQL Query Planner", new ArrayList<String>(), 4);
+
+        project.addStudent(student1);
+        project.addStudent(student2);
+
+
+        model.addAttribute("project", project);
+        model.addAttribute("student", student1);
+        return "student";
     }
 }
--- src/main/java/app/models/ProjectCoordinator.java
+++ src/main/java/app/models/ProjectCoordinator.java
@@ -1,3 +1,5 @@
+package app.models;
+
 public class ProjectCoordinator extends Professor {
 
 
