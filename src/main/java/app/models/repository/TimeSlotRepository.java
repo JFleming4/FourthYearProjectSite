555091d166a2c4eb4abe963a18bc78f38c4c3da1
JFleming4 <justinfleming95@gmail.com>
2018-03-15 11:43:49 -0400

    Added the entity and relations for TimeSlot

--- src/main/java/app/models/repository/TimeSlotRepository.java
+++ src/main/java/app/models/repository/TimeSlotRepository.java
@@ -4,6 +4,8 @@ import app.models.TimeSlot;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface TimeSlotRepository extends CrudRepository<TimeSlot, Long> {
     TimeSlot findFirstByOrderById();
