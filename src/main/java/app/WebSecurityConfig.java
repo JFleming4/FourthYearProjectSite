ada23c3e2493a5fdf818269fb6e33bec3069bda9
Justin Krol <justin.krol@shopify.com>
2018-03-22 13:34:43 -0400

    Add access denied page

--- src/main/java/app/WebSecurityConfig.java
+++ src/main/java/app/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +21,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService authenticatedUserDetailsService;
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -41,7 +45,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .permitAll();
+                .permitAll()
+                .and()
+            .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
     }
 
     @Autowired
--- src/main/java/app/controllers/AuthenticatedUserController.java
+++ src/main/java/app/controllers/AuthenticatedUserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -64,5 +65,10 @@ public class AuthenticatedUserController {
         UserDetails userDetails = userDetailsService.loadUserByUsername(currentUser.getUsername());
         model.addAttribute("currentUser", userDetails);
         return "welcome";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
     }
 }
