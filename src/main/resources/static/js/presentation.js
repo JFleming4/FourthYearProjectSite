a539ba8f18850f6336de0ec28a23b5a9b7ad4618
DerekStride <djgstride@gmail.com>
2018-04-03 09:56:34 -0400

    Build form with html instead of js

--- src/main/resources/static/js/presentation.js
+++ src/main/resources/static/js/presentation.js
@@ -3,8 +3,7 @@ $(document).ready(function () {
     // Get the timeslots associated with the project
     // Create the add TimeSlot form
     renderTimeSlots($("#projectId").text());
-    buildTimeSlotForm(true);
-
+    document.querySelector("#time-slot-submit").addEventListener("click", timeSlotSubmit);
 });
 
 var renderTimeSlots = function(projectId) {
@@ -40,36 +39,6 @@ var renderTimeSlot = function(div, timeSlot) {
     var br = $(document.createElement("br"));
     div.append(timeSlotElem).append(timeSlotLabel).append(br);
 }
-
-var buildTimeSlotForm = function(isProf) {
-    if(!isProf) return;
-    var allowedHours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
-    var allowedMinutes = ["00", "30"];
-    var allowedDay = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    var hourDropDown = buildSelect(allowedHours);
-    var minuteDropDown = buildSelect(allowedMinutes);
-    var dayDropDown = buildSelect(allowedDay);
-
-    var form = $(document.createElement("form"));
-    var hourLabel = $(document.createElement('span')).text("Hour");
-    var minuteLabel = $(document.createElement('span')).text("Minute");
-    var dayLabel = $(document.createElement('span')).text("Day");
-
-    var submit = $(document.createElement("input"))
-                    .attr("type", "submit")
-                    .attr("value", "Create")
-                    .attr("id", "time-slot-submit")
-                    .click(timeSlotSubmit);
-    var dayDiv = $(document.createElement("div")).append(dayLabel).append(dayDropDown);
-    var hourDiv = $(document.createElement("div")).append(hourLabel).append(hourDropDown);
-    var minDiv = $(document.createElement("div")).append(minuteLabel).append(minuteDropDown);
-    form.append(dayDiv)
-        .append(hourDiv)
-        .append(minDiv)
-        .append(submit);
-    $("#new-time-form").append(form);
-}
-
 
 var buildSelect = function(array) {
     var dropDown = $(document.createElement("select")).attr("class", "new-slot-input");
