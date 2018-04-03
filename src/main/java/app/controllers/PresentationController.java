174c5dd0126bda60437be884e97f200dd8e4b789
DerekStride <djgstride@gmail.com>
2018-04-03 10:58:08 -0400

    I defeated the js monster

--- src/main/java/app/controllers/PresentationController.java
+++ src/main/java/app/controllers/PresentationController.java
@@ -3,17 +3,13 @@ package app.controllers;
 import app.models.Day;
 import app.models.Project;
 import app.models.TimeSlot;
-import app.models.repository.ProfessorRepository;
 import app.models.repository.ProjectRepository;
-import app.models.repository.StudentRepository;
 import app.models.repository.TimeSlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,10 +18,12 @@ public class PresentationController {
     private ProjectRepository projectRepository;
     @Autowired
     private TimeSlotRepository timeSlotRepository;
+
     @GetMapping("/project/{id}/presentation")
     public String presentation(@PathVariable Long id,
                                Model model) {
-        model.addAttribute("projectId", id);
+
+        model.addAttribute("project", projectRepository.findById(id));
         return "presentation";
     }
     @GetMapping("/presentation/get-times")
@@ -41,14 +39,12 @@ public class PresentationController {
         }
         return times;
     }
-    @PostMapping("/presentation/new-time")
-    public String newTimeSlot(@RequestBody String req) {
-        String [] params = req.split("&");
-        String id = params[0].split("=")[1];
-        String day = params[1].split("=")[1];
-        String hour = params[2].split("=")[1];
-        String minute = params[3].split("=")[1];
-        Project project = projectRepository.findOne(new Long(id));
+    @PostMapping("/project/{id}/presentation/new-time")
+    public String newTimeSlot(@PathVariable Long id,
+                              @RequestParam("day") String day,
+                              @RequestParam("hour") String hour,
+                              @RequestParam("minute") String minute) {
+        Project project = projectRepository.findById(id);
         try {
             Day dayParam = Day.valueOf(day.toUpperCase());
             int hourParam = new Integer(hour);
@@ -62,6 +58,6 @@ public class PresentationController {
         } catch(Exception e) {
             e.printStackTrace();
         }
-        return "presentation";
+        return "redirect:/project/" + id + "/presentation";
     }
 }
